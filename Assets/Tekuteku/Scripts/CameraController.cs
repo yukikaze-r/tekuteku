a09fb078d8e3e915e0633c79de661a27f0adb8b9
yukikaze-r <ishibashi@sakurasoft.co.jp>
2015-04-28 23:45:15 +0900

    カメラ操作

---
+++ Assets/Tekuteku/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private bool isLastButtonDown = false;
+	private Vector3 lastPoint;
+
+	protected void Start () {
+	
+	}
+	
+	protected void Update () {
+		if (Input.GetMouseButton(0)) {
+			if(isLastButtonDown) {
+				var d = Input.mousePosition - lastPoint;
+				Debug.Log("d:"+d.x / Screen.width);
+				var rotationDegree = Camera.main.fieldOfView * d.x / Screen.width;
+				// http://0310unity.hateblo.jp/entry/unity_camera_circlemove_1
+			}
+			lastPoint = Input.mousePosition;
+			isLastButtonDown = true;
+		} else {
+			isLastButtonDown = false;
+		}
+	}
+}
