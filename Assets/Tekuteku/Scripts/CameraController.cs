4e3b26d7fdda72198d11103273f2c3f4f3124219
yukikaze-r <ishibashi@sakurasoft.co.jp>
2015-04-29 05:58:51 +0900

    カメラ操作

--- Assets/Tekuteku/Scripts/CameraController.cs
+++ Assets/Tekuteku/Scripts/CameraController.cs
@@ -1,22 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[@RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour {
 
 	private bool isLastButtonDown = false;
 	private Vector3 lastPoint;
+	private Camera camera;
 
 	protected void Start () {
-	
+		camera = GetComponent<Camera>();
+		transform.LookAt(transform.parent);
 	}
 	
 	protected void Update () {
 		if (Input.GetMouseButton(0)) {
 			if(isLastButtonDown) {
-				var d = Input.mousePosition - lastPoint;
-				Debug.Log("d:"+d.x / Screen.width);
-				var rotationDegree = Camera.main.fieldOfView * d.x / Screen.width;
-				// http://0310unity.hateblo.jp/entry/unity_camera_circlemove_1
+				Vector3 d = Input.mousePosition - lastPoint;
+				float dx = camera.fieldOfView * 3f * d.x / Screen.width;
+				float dy = camera.fieldOfView * 2f * d.y / Screen.height;
+				Vector3 xv = transform.localRotation * Vector3.left;
+				Vector3 yv = transform.localRotation * Vector3.up;
+				transform.parent.Rotate(xv, dy);
+				transform.parent.Rotate(yv, dx);
+				transform.LookAt(transform.parent);
 			}
 			lastPoint = Input.mousePosition;
 			isLastButtonDown = true;
